7d84909255504344e3570af4666e253dde2e2db0
Alissa Rao <lymia@lymia.moe>
2024-02-12 04:47:12 -0800

    Investigation

--- modules/default.nix
+++ modules/default.nix
@@ -1,19 +1,19 @@
 { lib, ... }:
 
 {
-  imports = [
-    ./files.nix
-    ./hotkeys.nix
-    ./shortcuts.nix
-    ./spectacle.nix
-    ./windows.nix
-    ./workspace.nix
-    ./kwin.nix
-    ./startup.nix
-    ./panels.nix
-  ];
+  #imports = [
+  #  ./files.nix
+  #  ./hotkeys.nix
+  #  ./shortcuts.nix
+  #  ./spectacle.nix
+  #  ./windows.nix
+  #  ./workspace.nix
+  #  ./kwin.nix
+  #  ./startup.nix
+  #  ./panels.nix
+  #];
 
-  options.programs.plasma.enable = lib.mkEnableOption ''
-    Enable configuration management for KDE Plasma.
-  '';
+  #options.programs.plasma.enable = lib.mkEnableOption ''
+  #  Enable configuration management for KDE Plasma.
+  #'';
 }
